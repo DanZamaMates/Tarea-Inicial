6b8ecf1c262b6b7fb8bb852aa2dc53d2c33d4a49
DanZamaMates <58340742+DanZamaMates@users.noreply.github.com>
2020-09-14 12:41:07 -0500

    Update Inicial.py

--- Inicial.py
+++ Inicial.py
@@ -1,10 +1,19 @@
-#matricula y nombre 1
-#matricula y nombre 2
-#proposito del programa
+#A01570576 Daniel De Zamacona
+#Elmer Osiel A00826359
+#Generar el area y perimetro del rectangulo
 
 #funcion1  calcular área del rectángulo
+<<<<<<< HEAD
+print("Ingresar base=")
+base = float(input())
+print("Ingresar altura=")
+altura = float(input())
+function1= float(base)*float(altura) 
+
+=======
 def calcularArea(a,b):
     return print(str(a*b))
+>>>>>>> funciones
 
 #funcion2  calcular perímetro del rectángulo
 def calcularPerimetro(a,b):
@@ -20,7 +29,11 @@ print("medida de un lado del rectángulo")
 l2 = float(input())
 
 #desplegar calculo funcion1
+<<<<<<< HEAD
+print(function1)
+=======
 calcularArea(l1,l2)
+>>>>>>> funciones
 #desplegar calculo funcion 2
 calcularPerimetro(l1,l2)
 
