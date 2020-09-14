235fb233b071fb199264578c612ea9a0f8234129
DanZamaMates <58340742+DanZamaMates@users.noreply.github.com>
2020-09-14 12:33:09 -0500

    Update Inicial.py

--- Inicial.py
+++ Inicial.py
@@ -3,7 +3,8 @@
 #proposito del programa
 
 #funcion1  calcular área del rectángulo
-
+def calcularArea(a,b):
+    return print(str(a*b))
 
 #funcion2  calcular perímetro del rectángulo
 def calcularPerimetro(a,b):
@@ -19,7 +20,7 @@ print("medida de un lado del rectángulo")
 l2 = float(input())
 
 #desplegar calculo funcion1
-
+calcularArea(l1,l2)
 #desplegar calculo funcion 2
 calcularPerimetro(l1,l2)
 
